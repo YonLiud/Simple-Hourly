b68f0a35c1213e95582920bc41c92eaefa452f4e
Yon Liud <45634836+YonLiud@users.noreply.github.com>
2020-08-16 20:47:54 +0300

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,41 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+
+namespace Client
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+            EventHandlers["recieveMessage"] += new Action<string>(OnMassageRecieve);
+        }
+        private void OnClientResourceStart(string resourceName)
+        {
+            Cooldown();
+        }
+        private void OnMessageRequest()
+        {
+
+        }
+        private void Cooldown()
+        {
+            TriggerServerEvent("serverStartMessage");
+        }
+        private void OnMassageRecieve(string message)
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 138, 43, 226 },
+                multiline = true,
+                args = new[] { "Midnight RP", $"^* ^4{message}" }
+            });
+            return;
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,69 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server : BaseScript
+    {
+        string message;
+        bool isRunning = false;
+        public Server()
+        {
+            EventHandlers["serverStartMessage"] += new Action(OnStartService);
+        }
+        public void OnStartService()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                OnCooldown();
+            }
+        }
+        public async void OnCooldown()
+        {
+            await Delay(420000);
+            OnMessage();
+
+        }
+        public void OnMessage()
+        {
+            Random random = new Random();
+            int value = random.Next(1, 6);
+            if(value == 1)
+            {
+                message = "Dont Forget to Sign Up For our CAD";
+            }
+            else
+            {
+                if (value == 2)
+                {
+                    message = "Dont Forget to join our discord at discord.gg/Tx9MGVY";
+                }
+                else
+                {
+                    if(value == 3)
+                    {
+                        message = "Make sure to read the rules in our discord at #rules in our discord";
+                    }
+                    else
+                    {
+                        if(value == 4)
+                        {
+                            message = "Considering joining a department? Apply Today at our Discord!";
+                        }
+                        else
+                        {
+                            message = "Make sure to read the rules in our discord at #rules in our discord";
+                        }
+                    }
+                }
+            }
+            TriggerClientEvent("recieveMessage", message);
+            OnCooldown();
+        }
+    }
+}
